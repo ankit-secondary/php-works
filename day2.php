631a92a66a9de9115e06766ee5bf3bbde3c7ff09
zapbuild-ankit <fbg.ankit@gmail.com>
2020-01-17 09:45:13 +0530

    changes

--- day2.php
+++ day2.php
@@ -1,12 +1,12 @@
 <?php
+// class
 class company {
+    //Access modifier ,variables, functions
     public $Name;
      public $Addressc;
       function set_Name($Name){
                  $this->Name = $Name;
       }
-    // function _construct($Address){
-    //$this->Addressc = $Addressc;
     function displayc($Addressc){
         $this->Addressc = $Addressc;
         echo " Address of the company is ". $Addressc;
@@ -20,9 +20,11 @@ $contain=new company();
  $contain->set_Name("Zapbuild-technology");
  echo $contain->get_Name();
  echo "<br>";
+ //  used 2nd class for inheritance
  class client extends company{
      public $name;
      public $address;
+     // used constructor
     public function __construct($name,$address){
         $this->name=$name;
         $this->address=$address;
@@ -31,6 +33,7 @@ $contain=new company();
             echo "Name of client is ".$this->name ."<br> Address of the client is ".$this->address;
         }
         }
+        // making objects of the class
     $client=new client("Ankit","chandigarh");
      $client->displayc('mohali');
      $client->display();
