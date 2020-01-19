d03023d5177969846e2c6444a8f0b94a83aebbf2
zapbuild-ankit <fbg.ankit@gmail.com>
2020-01-19 11:01:43 +0530

    used Namespace

--- Namespace.php
+++ Namespace.php
@@ -1,9 +1,15 @@
 <?php
+
 //using namespace
+
 namespace myname{
+    //global variables
+
  $id;
 $id;
+
 //making first class
+
 class first{
     public $id;
     function show($id){
@@ -20,12 +26,19 @@ class second extends first{
     }
 }
 //making object of first class
+
 $first=new first();
+
 //making object of second class
+
 $second=new second();
+
 //calling 'show' method of first class
+
 $first->show("1234");
+
 //calling 'dispaly' method of second class
+
 $second->display("4567");
 }
 ?>
