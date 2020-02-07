5a0d53b9600702aaead80fe17067c7a9be106315
zapbuild-ankit <fbg.ankit@gmail.com>
2020-02-07 14:08:46 +0530

    session2 page

---
+++ session2.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+</head>
+<body>
+
+<!-- Form for mail  and password-->
+
+<form action="session1.php" method="post">
+<input type="Text" name="mail" value="Mailid"><br>
+<input type="password" name="password" value="password"><br>
+<input type="submit" name="submit" value="submit">
+</form>
+</body>
+</html>
